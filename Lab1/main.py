0383a427be8da93a8cb81982e748f8e864158c14
Jeronimoyang <1507213842@qq.com>
2025-03-10 12:16:15 +0800

    update

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,188 +1,91 @@
-from dataloader import DataLoader
+from Dataloader import DataLoader
 from naive import Naive
-from minHash import MinHash
+from minhash import MinHash
 import random
 import time
 
+n_samples=500
+c=0.9
+FILE_PATH='./data/E1_kosarak_100k.txt'
 
-# --------------- 参数设置 --------------- #
-n_samples = 500 # 采样数
-b = 10           # MinHash 方法的 band 数量
-r = 10           # MinHash 方法的 row 数量
-# 记录最优 band 和 row 的参数
-best = {"b": None, "r": None, "result": None, "value": 100000}
-c = 0.9         # Naive 方法的阈值
-FILE_PATH = './data/E1_kosarak_100k.txt' # 数据集路径
-
-# --------------- 数据加载 --------------- #
-# 读取数据集，将相同标号的元素放在一个集合中
-# 最终将整个数据集，用若干个集合表示
 def data(file_path):
-    # 打印数据加载信息
     print('Data Loading...')
-    # 设置时间起点
-    time_start = time.time()
-    # 调用 DataLoader 类的 load 方法加载数据
-    corpus = DataLoader(file_path).load()
-    # 设置时间终点
-    time_end = time.time()
-    # 打印数据加载信息
+    time_start=time.time()
+    corpus=DataLoader(file_path).load()
+    time_end=time.time()
     print('Data Loaded!')
-    # 打印数据集大小和加载时间
-    print(f'Number of Set: {len(corpus)}')
-    print(f'Time: {time_end - time_start}s')
-    # 返回列表 corpus，每个元素是一个集合
+    print(f'Number of Set:{len(corpus)}')
+    print(f'Time:{time_end-time_start}s')
     return corpus
 
-# --------------- 采样 --------------- #
-# 从若干个集合中随机采样 n_samples 个集合
-def sample(corpus, n_samples):
-    # 打印采样信息
+def sample(corpus,n_samples):
     print('Random Sampling...')
-    # 设置时间起点
-    time_start = time.time()
-    # 使用 random.sample 方法从 corpus 中随机采样 n_samples 个样本
-    samples = random.sample(corpus, n_samples)
-    # 设置时间终点
-    time_end = time.time()
-    # 打印采样信息
-    print(f'Number of Samples: {n_samples}')
+    time_start=time.time()
+    samples=random.sample(corpus, n_samples)
+    time_end=time.time()
+    print(f'Number of Samples:{n_samples}')
     print('Sampling Done!')
-    print(f'Time: {time_end - time_start}s')
-    # 返回列表 samples，每个元素是一个集合
+    print(f'Time:{time_end-time_start}s')
     return samples
 
-# --------------- navie 方法 --------------- #
-# 使用暴力方法计算相似集合对的数量
-def naiveMethod(samples, c):
-    # 打印 Naive 方法信息
+def naiveMethod(samples,c):
     print('Naive Method Running...')
-    # 设置时间起点
-    time_start = time.time()
-    # 调用 Naive 类的 run 方法
-    naive = Naive()
-    naive_result = naive.run(samples, c)
-    # 设置时间终点
-    time_end = time.time()
-    # 打印 Naive 方法信息
-    print(f'Naive Method Result: {len(naive_result)}')
-    print(f'Time: {time_end - time_start}s')
-    # 返回相似集合对的数量
+    time_start=time.time()
+    naive=Naive()
+    naive_result=naive.run(samples,c)
+    time_end=time.time()
+    print(f'Naive Method Result:{len(naive_result)}')
+    print(f'Time:{time_end-time_start}s')
     return len(naive_result)
 
-# --------------- 数据预处理 --------------- #
-# 汇总所有集合的元素种类，并将元素进行排序，生成对应的索引，
-# 再用索引值代替元素，表示集合的特征
-# 最终返回：用索引值表示的集合数据；元素种类的数量
-def preProcess(corpus):
+def create_data(sample):
+    print('Discretization Running...')
     # 设置时间起点
-    print('Preprocess...')
-    time_start = time.time()
-    # data1 和 data2 用于存储合并后的数据集，flag 用于控制数据合并的轮次
-    data1 = []
-    data2 = []
-    flag = 0
-    # 将 corpus 中的集合两两合并，并去重，合并后的集合添加到 data1 中
-    for i in range(0, len(corpus) - 1, 2):
-        data1.append(list(set(corpus[i]).union(set(corpus[i + 1]))))
-    # 如果 corpus 的长度为奇数，将最后一个集合添加到 data1 中
-    if len(corpus) % 2 != 0:
-        data1.append(list(set(corpus[-1])))
-    # 对合并后的集合进行处理，直到只剩下一个集合
-    while True:
-        # 如果 flag 为 0
-        if flag == 0:
-            # 遍历 data1 中的集合，两两合并，并去重，合并后的集合添加到 data2 中
-            for j in range(0, len(data1) - 1, 2):
-                data2.append(list(set(data1[j]).union(set(data1[j + 1]))))
-            # 如果 data1 的长度为奇数，将最后一个集合添加到 data2 中
-            if len(data1) % 2 != 0:
-                data2.append(list(set(data1[-1])))
-            # 如果 data2 的长度为 1
-            if len(data2) == 1:
-                # 打乱 data2 中的元素顺序
-                random.shuffle(data2[0])
-                # 将 data2 中的元素赋值给 values
-                values = data2[0]
-                # 跳出循环
-                break
-            # 将 data1 置空
-            data1 = []
-            # 将 flag 设置为 1
-            flag = 1
-        # 如果 flag 为 1
-        if flag == 1:
-            # 遍历 data2 中的集合，两两合并，并去重，合并后的集合添加到 data1 中
-            for j in range(0, len(data2) - 1, 2):
-                data1.append(list(set(data2[j]).union(set(data2[j + 1]))))
-            # 如果 data2 的长度为奇数，将最后一个集合添加到 data1 中
-            if len(data2) % 2 != 0:
-                data1.append(list(set(data2[-1])))
-            # 如果 data1 的长度为 1
-            if len(data1) == 1:
-                # 打乱 data1 中的元素顺序
-                random.shuffle(data1[0])
-                # 将 data1 中的元素赋值给 values
-                values = data1[0]
-                # 跳出循环
-                break
-            # 将 data2 置空
-            data2 = []
-            # 将 flag 设置为 0
-            flag = 0
-    # 将 values 中的元素进行排序
-    values = sorted(list(values))
-    # 创建字典 value2index，用于存储 values 中的元素和其对应的索引
-    value2index = dict()
-    # 遍历 values 中的元素，将元素和其索引添加到 value2index 中
-    for i, element in enumerate(values):
-        value2index[element] = i
-    # 创建列表 data，用于存储处理后的数据集
-    data = [[] for _ in range(len(corpus))]
-    # 遍历 corpus 中的集合，将集合中的元素转换为其在 values 中的索引
-    for i in range(len(corpus)):
-        for element in corpus[i]:
-            data[i].append(value2index[element])
+    time_start=time.time()
+    # 创建一个空集合
+    S=set()
+    # 遍历所有集合
+    for i in range(len(sample)):
+        # 遍历集合中的元素
+        for j in range(len(sample[i])):
+            # 将集合中的元素加入到集合S中，集合S中的元素不会重复
+            S.add(sample[i][j]) 
+    # 将集合S中的元素转换成列表
+    num=list(S)
+    # 遍历所有集合
+    for i in range(len(sample)):
+        # 遍历集合中的元素
+        for j in range(len(sample[i])):
+            # 遍历 num 列表，找到当前元素对应的索引
+            for k in range(len(num)):
+                # 如果当前元素等于 num 中的元素
+                if sample[i][j]==num[k]:
+                    # 将当前元素替换成 num 中的索引
+                    sample[i][j]=k;
     # 设置时间终点
-    time_end = time.time()
-    print('Done!')
-    print(f'Time: {time_end - time_start}s')
-    # 返回处理后的数据集 data 和 values 的长度
-    return data, len(values)
+    time_end=time.time()
+    print(f'Time:{time_end-time_start}s')
+    print('Discretization Done')
+    # 返回用索引表示的集合和元素种类数
+    return sample,len(num)
 
-# --------------- MinHash 方法 --------------- #
-def minHashMethod(samples, n, b, r):
-    # 打印 MinHash 方法信息
+def MinHashMethod(tot,sample,c):
     print('MinHash Method Running...')
     # 设置时间起点
-    time_start = time.time()
-    # 调用 MinHash 类的 run 方法
-    minHash = MinHash(b, r)
-    minHash_result = minHash.run(samples, n)
+    time_start=time.time()
+    mh=MinHash()
+    mh_result=mh.minhash_work(tot,sample,c)
     # 设置时间终点
-    time_end = time.time()
-    # 打印 Min
-    print(f'b = {b} r = {r}')
-    print(f'MinHash Method Result: {minHash_result}')
-    print(f'Time: {time_end - time_start}s')
+    time_end=time.time()
+    print(f'MinHash Method Result:{len(mh_result)}')
+    print(f'Time:{time_end-time_start}s')
+    return len(mh_result)
 
-# --------------- 参数设置 --------------- #
-def main():
-    # 加载数据
-    print('............................................................')
-    corpus = data(FILE_PATH)
-    # 采样
-    print('............................................................')
-    samples = sample(corpus, n_samples)
-    # Naive 方法
-    print('............................................................')
-    naive_result = naiveMethod(samples, c)
-    # 数据预处理
-    print('............................................................')
-    processed, n_elements = preProcess(samples)
-    # MinHash 方法
-    print('............................................................')
-    minHashMethod(processed, n_elements, b, r)
+if __name__=='__main__':
+    corpus=data(FILE_PATH)
+    samples=sample(corpus,n_samples)
+    samples,tot=create_data(samples)
+    naive_result=naiveMethod(samples,c)
+    mh_result=MinHashMethod(tot,samples,c)
 
-if __name__ == '__main__':
-    main()
+
--- Lab1/minhash.py
+++ Lab1/minhash.py
@@ -1,122 +1,59 @@
 import numpy as np
-import hashlib
+import matplotlib.pyplot as plt
 
-# 实现 MinHash 和局部敏感哈希（LSH）算法
-# 估计集合之间的相似度
 class MinHash:
-    # --------------- 初始化 MinHash 类 --------------- #
-    def __init__(self, b, r):
-        self.b = b
-        self.r = r
-
-    # --------------- 静态方法，不依赖于类的实例 self --------------- #
-    # 进行一次随机排列，打乱索引值，计算每个集合的 MinHash 值
+    # 初始化函数
+    def __init__(self):
+        pass
+    
     @staticmethod
-    # 对数据应用单一哈希函数进行处理，用来生成单一的 MinHash 值
-    def singleHash(data, n):
-        # 创建一个长度为 n 的序列
-        seq = [i for i in range(n)]
-        # 创建一个长度为 len(data) 的列表，用来存储 MinHash 值
-        result = [n for _ in range(len(data))]
-        # 生成一个随机排列的索引序列，相当于一个哈希函数
-        seq = np.random.permutation(seq)
-        # 遍历 data 中的每个集合，找到每个集合中的最小哈希值
-        for i in range(len(result)):
-            # 初始化 minhash 为 n 最大值
-            minhash = n
-            # 遍历集合中的每个元素
-            for element in data[i]:
-                # 如果元素的哈希值小于 minhash
-                if seq[element] < minhash:
-                    result[i] = element
-                    minhash = seq[element]
-        # 返回 MinHash 值
-        return result
+    def minhash_work(n_element,sample,c):
+        # 创建一个空列表，用于存储相似集合对
+        R=set()
+        # 创建长度为500*500的列表，用于存储相似集合对的数量
+        ans=[0 for i in range(500*500)]
+        # 采用多次哈希 50次
+        for _ in range(50):
+            # 生成长度为 n_element 的索引列表
+            per=[i for i in range(n_element)]
+            # 对索引列表进行随机排列
+            per=np.random.permutation(per)
+            # 创建长度为 n_element 的列表，用于存储索引
+            pos=[0 for i in range(n_element)]
+            # 遍历索引列表
+            for i in range(n_element):
+                # 记录元素在新的排列的位置
+                pos[per[i]]=i
+            # 创建长度为 len(sample) 的列表，用于存储最小哈希值
+            res=[10**8 for i in range(len(sample))]
+            # 遍历所有集合
+            for i in range(len(sample)):
+                # 创建一个变量，用于存储当前集合的最小哈希值
+                mn=10**8
+                # 遍历集合中的元素
+                for j in range(len(sample[i])):
+                    # 如果当前元素的哈希值小于当前集合的最小哈希值
+                    if pos[sample[i][j]]<mn:
+                        # 更新当前集合的最小哈希值
+                        mn=pos[sample[i][j]] 
+                # 将当前集合的最小哈希值存入列表中
+                res[i]=mn
+            # 遍历所有集合
+            for i in range(len(sample)-1):
+                # 遍历所有集合
+                for j in range(i+1,len(sample)):
+                    # 如果两个集合的最小哈希值相等
+                    if res[i]==res[j]:
+                        # 记录相似集合对(i,j)
+                        ans[(i-1)*len(sample)+j]=ans[(i-1)*len(sample)+j]+1
+        # 遍历所有集合
+        for i in range(len(sample)-1):
+            # 遍历所有集合
+            for j in range(i+1,len(sample)):
+                # 如果两个集合的相似度大于阈值
+                if(ans[(i-1)*len(sample)+j]>=c*50):
+                    # 记录相似集合对(i,j)
+                    R.add((i,j))
+        # 返回相似集合对
+        return R
 
-    # --------------- 计算 sigMatrix --------------- #
-    # 进行随机排列，打乱索引值，计算每个集合的 MinHash 值
-    # 重复 n_hash_funcs 次，生成 sigMatrix，即签名矩阵
-    # 签名矩阵的每一行对应一个 MinHash 值列表
-    def sigMatrix(self, data, n_elements, n_hash_funcs):
-        # 初始化结果列表
-        result = []
-        # 循环 n_hash_funcs 次，每次生成一个 MinHash 值列表
-        for i in range(n_hash_funcs):
-            # 生成一个 MinHash 值列表
-            single = self.singleHash(data, n_elements)
-            # 将 MinHash 值列表添加到结果列表中
-            result.append(single)
-        # 返回结果列表，转换为 NumPy 矩阵
-        return np.array(result)
-
-    # 计算 MinHash
-    def minHash(self, data, n_elements):
-        # 初始化哈希桶
-        hashBuckets = {}
-        # 表示MinHash函数的数量，b是band的数量，r是每个band的行数
-        n = self.b * self.r
-        # 计算 sigMatrix，n 行，len(data)列，每行对应一个 MinHash 值
-        sigMatrix = self.sigMatrix(data, n_elements, n)
-        # 初始化 begin 和 end
-        begin, end = 0, self.r
-        count = 0
-        # 遍历 sigMatrix 的每一行，sigMatrix.shape[0] 表示 sigMatrix 的行数
-        while end <= sigMatrix.shape[0]:
-            # 当前的 band 编号
-            count += 1
-            # 在每个 band 中，遍历 sigMatrix 的每一列
-            for colNum in range(sigMatrix.shape[1]):
-                # 创建一个 MD5 哈希对象
-                hashObj = hashlib.md5()
-                # 提取当前 band 的值，转换为字符串，计算哈希值
-                band = str(sigMatrix[begin: begin + self.r, colNum]) + str(count)
-                # 将 band 字符串编码成字节格式，接着更新哈希对象，加入要计算哈希的内容
-                hashObj.update(band.encode())
-                # 计算并返回 band 的哈希值
-                tag = hashObj.hexdigest()
-                # 检查哈希桶中是否存在这个哈希值 tag
-                if tag not in hashBuckets:
-                    # 如果不存在，创建一个新的哈希桶，tag，即哈希值，作为键；colNum，即集合编号，作为值
-                    # 每个哈希值对应着一个或多个集合
-                    hashBuckets[tag] = [colNum]
-                # 如果哈希桶中已经存在这个哈希值 tag
-                elif colNum not in hashBuckets[tag]:
-                    # 将 colNum，即集合编号，添加到哈希桶中
-                    hashBuckets[tag].append(colNum)
-            # 更新 begin 和 end，进入下一个 band
-            begin += self.r
-            end += self.r
-        # 返回哈希桶，即哈希值 tag 和集合编号 colNum 的映射
-        return hashBuckets
-
-    # --------------- 计算 MinHash + LSH，相似集合对的数量 --------------- #
-    def run(self, data, n):
-        # 调用 minHash 方法，返回哈希桶，每个哈希桶对应一个哈希值 tag 和集合编号 colNum 的映射
-        hashBucket = self.minHash(data, n)
-        # 初始化结果列表，存储每个集合的相似集合编号
-        #result = [set() for _ in range(len(data))]
-        # 初始化已处理过的配对
-        processed_pairs = set()
-        # 遍历每个哈希桶
-        for key in hashBucket:
-            # 遍历哈希桶中的所有文档编号col1
-            for col1 in hashBucket[key]:
-                # 遍历哈希桶中的所有文档编号col2
-                for col2 in hashBucket[key]:
-                    # 如果 col1 == col2，将集合本身排除
-                    if col1 == col2:
-                        continue
-                    sorted_pair = tuple(sorted((col1, col2)))
-                    if sorted_pair in processed_pairs:
-                        continue
-                    # 将 col2 添加到 col1 的相似文档集合中
-                    # 因为在一个哈希桶中，相同的哈希值对应着相似的集合
-                    #result[col1].add(col2)
-                    #print(col1, col2)
-                    #num = num + 1
-                    # 将已处理过的配对添加到 processed_pairs 中
-                    processed_pairs.add(sorted_pair)
-        # 计算总相似配对数
-        sum = len(processed_pairs)
-        # 返回相似配对数
-        return sum
