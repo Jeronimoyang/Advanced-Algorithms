90efe7a95ce146f1f3b95f4eb57fe9701ce01dd8
Jeronimoyang <1507213842@qq.com>
2025-03-17 23:56:12 +0800

    update

--- Lab2/createData.py
+++ Lab2/createData.py
@@ -6,10 +6,10 @@ class DataGenerator:
         # 生成n个均匀分布、正态分布和Zipf分布的随机数
         # 定义 self.dict 字典，存储三种分布的 n 个随机数
         # uniform：生成 n 个均匀分布的随机数，范围为 [1, 1000]
-        # normal：生成 n 个正态分布的随机数，均值为 500，标准差为 200
+        # normal：生成 n 个正态分布的随机数，均值为 500，标准差为 100
         # zipf：生成 n 个 Zipf 分布的随机数，参数为 1.2
         self.dict = {"uniform": np.random.uniform(1, 1001, n).astype(int).tolist(),
-                     "normal": np.random.normal(500, 200, n).astype(int).tolist(),
+                     "normal": np.random.normal(500, 100, n).astype(int).tolist(),
                      "zipf": np.random.zipf(1.2, n).astype(int).tolist()}
     # --------------- 加载函数 --------------- #
     def load(self):
